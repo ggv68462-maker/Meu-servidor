6d98958cba7bf0c8b77cad6b5de3776e3ac1d7e8
ggv68462-maker <ggv68462@gmail.com>
2026-06-15 06:52:12 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -4,18 +4,51 @@ const app = express();
 app.use(express.json());
 
 let mensagens = [];
+let estoque = [];
 
+// STATUS
 app.get("/", (req, res) => {
-  res.send("Servidor online!");
+res.send("Servidor online!");
 });
 
+// CHAT
 app.get("/mensagens", (req, res) => {
-  res.json(mensagens);
+res.json(mensagens);
 });
 
 app.post("/mensagens", (req, res) => {
-  mensagens.push(req.body);
-  res.json({ ok: true });
+mensagens.push(req.body);
+res.json({ ok: true });
+});
+
+// ESTOQUE
+app.get("/estoque", (req, res) => {
+res.json(estoque);
+});
+
+app.post("/estoque", (req, res) => {
+const link = req.body.link;
+
+if (!link) {
+return res.status(400).json({
+ok: false,
+erro: "link não informado"
+});
+}
+
+const existe = estoque.find(item => item.link === link);
+
+if (!existe) {
+estoque.push({
+link: link,
+data: Date.now()
+});
+}
+
+res.json({
+ok: true,
+total: estoque.length
+});
 });
 
 app.listen(process.env.PORT || 3000);
