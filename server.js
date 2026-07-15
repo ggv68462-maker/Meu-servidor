5f50cf62ca978437231e04fa1af59e02434467e3
ggv68462-maker <ggv68462@gmail.com>
2026-07-15 19:35:08 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -6,36 +6,39 @@ app.use(express.text({ type: '*/*' }));
 const PORT = process.env.PORT || 3000;
 const conexoes = {};
 
-// Captura a URL bruta que vem do Kodular
+// Captura o link que vem do Kodular
 app.get('/', (req, res) => {
-    const textoBruto = req.url.split('?')[1];
+    // Verifica se a URL do aplicativo contém o ponto de interrogação
+    if (req.url.includes('?')) {
+        // Pega EXATAMENTE o texto puro que está depois do "?"
+        const textoPuro = req.url.split('?')[1];
 
-    if (textoBruto) {
-        conexoes[textoBruto] = res;
+        if (textoPuro) {
+            conexoes[textoPuro] = res;
 
-        setTimeout(() => {
-            if (conexoes[textoBruto]) {
-                conexoes[textoBruto].status(200).send("");
-                delete conexoes[textoBruto];
-            }
-        }, 60000);
-        return;
+            // Limpa da memória após 60 segundos se o Termux não responder
+            setTimeout(() => {
+                if (conexoes[textoPuro]) {
+                    conexoes[textoPuro].status(200).send("");
+                    delete conexoes[textoPuro];
+                }
+            }, 60000);
+            return;
+        }
     }
     res.send("Online");
 });
 
-// Entrega a lista para o Termux
+// Entrega a lista com os textos puros para o Termux ler
 app.get('/termux/pendentes', (req, res) => {
     res.status(200).json(Object.keys(conexoes));
 });
 
-// Recebe do Termux e repassa ao App
+// Remove o comando da memória caso queira limpar manualmente
 app.post('/termux/resposta/:chave', (req, res) => {
     const chave = req.params.chave;
-    const conteudo = req.body;
-
     if (conexoes[chave]) {
-        conexoes[chave].status(200).send(conteudo);
+        conexoes[chave].status(200).send("");
         delete conexoes[chave];
     }
     res.status(200).send("");
